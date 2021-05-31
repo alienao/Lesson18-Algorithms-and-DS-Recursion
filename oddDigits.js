2850d5a52056770410324427abbdf92fb4eb8b64
alienao <armin.orujyan@gmail.com>
2021-05-31 22:51:16 +0400

    build: oddDigits.js

---
+++ oddDigits.js
@@ -0,0 +1,17 @@
+// 1. Write a recursive function to determine whether all digits of the number are odd or not.
+
+function oddDigits(number) {
+  if (!number) {
+    return true;
+  }
+  if (number % 2 === 0) {
+    return false;
+  }
+
+  return oddDigits(Math.floor(number / 10));
+}
+
+console.log(oddDigits(4211133));
+console.log(oddDigits(7791));
+console.log(oddDigits(5));
+
